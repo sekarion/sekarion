e98ab8b7f1203eda25814647f1ef4019af150a9a
jorisvidéo <joris1904@live.fr>
2019-12-29 20:35:07 +0100

    - fix ping and upgrade ping :)

--- src/utils/UpdateStatus.js
+++ src/utils/UpdateStatus.js
@@ -53,88 +53,18 @@ class UpdateStatus {
     async updateService(maxrun, ip, port){
         let timestart = new Date().getTime();
         let self = this;
-        return new Promise(function(resolve, reject) {
-            let link;
-            switch (port) {
-                case 21:
-                    //add ftp if doesn't exist
-                    if(!/^((ftp):\/\/)/.test(ip)) {
-                        link = `ftp://${ip}:${port}`;
-                    }else{
-                        link = ip;
-                    }
-                    break;
-                case 23:
-                    //add ftp if doesn't exist
-                    if(!/^((smtp):\/\/)/.test(ip)) {
-                        link = `ftp://${ip}:${port}`;
-                    }else{
-                        link = ip;
-                    }
-                    break;
-                case 80:
-                    //add http if doesn't exist
-                    let pt = /^((http):\/\/)/;
-                    if(!pt.test(ip)) {
-                        link = `http://${ip}`;
-                    }else{
-                        link = ip;
-                    }
-                    break;
-                case 110:
-                    //add http if doesn't exist
-                    if(!/^((pop3):\/\/)/.test(ip)) {
-                        link = `pop3://${ip}`;
-                    }else{
-                        link = ip;
-                    }
-                    break;
-                case 443:
-                    //replace if https doesn't exist
-                    let pattern = /^((https):\/\/)/;
-                    if(!pattern.test(ip)) {
-                        link = `https://${ip}`;
-                    }else{
-                        link = ip;
-                    }
-                    break;
-                case 465:
-                    //add http if doesn't exist
-                    if(!/^((smtps):\/\/)/.test(ip)) {
-                        link = `smtps://${ip}`;
-                    }else{
-                        link = ip;
-                    }
-                    break;
-                case 995:
-                    //add http if doesn't exist
-                    if(!/^((pop3):\/\/)/.test(ip)) {
-                        link = `pop3://${ip}:${port}`;
-                    }else{
-                        link = `${ip}:${port}`;
-                    }
-                    break;
-                default:
-                    link = `${ip}:${port}`;
-                    break;
+        return new Promise(async (resolve, reject) => {
+            //if error default is false and calculate the time and resolve
+            try{
+                await curl.PingService(ip, port,function(inUse) {
+                    self.time = self.msToMS(Date.now()-timestart);
+                    self.status = inUse;
+                });
+            }catch (e) {
+                self.time= self.msToMS(Date.now()-timestart);
+                self.status = false;
             }
-            curl.curl_get(link, false, true, null, false, false,async(statusCode, data, headers) => {
-                self.time = self.msToMS(new Date().getTime() - timestart);
-                //close curl
-                if(statusCode.toString() === "Error: Couldn't resolve host name"){
-                    //return false because false host
-                    self.error = statusCode;
-                    self.status = false;
-                    resolve(self.status);
-                }else{
-                    self.status = statusCode >= 200 && statusCode <= 399;
-                }
-                if (!self.status && self.run < maxrun) {
-                    self.run = self.run + 1;
-                    return await self.updateService(maxrun, ip, port);
-                }
-                return resolve(self.status);
-            });
+            resolve(self.status);
         });
     }
     /**
--- src/utils/curl.js
+++ src/utils/curl.js
@@ -49,7 +49,19 @@ module.exports.curl_get = function(href, header = false, body = true, timeout = 
     //close the request after
     //curl.close();
 };
-
+/**
+ * @param {String|number} ip - ip of host
+ * @param {string} port - port of host
+ * @param {function} callback
+ */
+module.exports.PingService = async function (ip, port, callback){
+    let tcpPortUsed = require('tcp-port-used');
+    //convert host to ip
+    if (!checkifip(ip)){
+        ip = await lookupdns(ip);
+    }
+    await tcpPortUsed.check(port, ip).then(callback);
+};
 /**
  * Check the current servers ping status
  * @param {Number|String} ip
@@ -71,3 +83,27 @@ module.exports.Ping = function(ip, max_runs, run = 1, callback) {
         return Ping(ip, max_runs, run + 1);
     }*/
 };
+
+/**
+ *
+ * Check if is valid IPV4
+ * @param {string} ip
+ * @return {Boolean}
+ **/
+function checkifip(ip) {
+    const v4 = '(?:25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)(?:\\.(?:25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)){3}';
+    return new RegExp(`^${v4}$`).test(ip);
+}
+/**
+ * convert domain/host to ip
+ * @param {string} ip - domain/host to convert
+ * @return {string} ip converted
+ **/
+async function lookupdns(ip) {
+    const dns = require('dns');
+    return new Promise(async (resolve, reject) => {
+        dns.lookup(ip, function (err, address, family) {
+            resolve(address);
+        });
+    });
+}
